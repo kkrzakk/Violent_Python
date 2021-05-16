fe0ce6039fa074392201083798cdb7dfd5fe8c0d
kkrzakk <adam.krzaczek@gmail.com>
2021-05-16 15:30:29 -0400

    Updated zip_cracker.py; new file: full_connect_scan.py

--- full_connect_scan.py
+++ full_connect_scan.py
@@ -1,47 +1,50 @@
 import optparse
-import socket
+from socket import *
 
 def connection_Scan(target_host, target_port):
 	try:
-		connection = socket.socket(AF_INET, SOCK_STREAM)
+		connection = socket(AF_INET, SOCK_STREAM)
 		connection.connect((target_host,target_port))
-		connection.send('Testing Connction')
+		connection.send('Testing Connection')
 		results = connection.recv(100)
 		print('[+] %d - tcp open [+]'% target_port)
+		print('[+] %s'% str(results))
 		connection.close()
-	except:
+	except Exception as e:
+		print(e)
 		print('[+] %d - tcp closed [+]'% target_port)
 
 def port_scan(target_host, target_ports):
 	try:
-		target_IP = socket.gethostbyname(target_host)
+		target_IP = gethostbyname(target_host)
 	except:
 		print('[-] Unknown Host [-]')
 		return
 	try:
-		target_name=socket.gethostbyaddr(target_IP)
+		target_name=gethostbyaddr(target_IP)
 		print('[+] Scan Results for: %s [+]'% target_Name[0])
 	except:
 		print('[+] Scan Results for: %s [+]'% target_IP)
 	setdefaulttimeout(1)
 	for target_port in target_ports:
-		print('Scanning port %d'% target_port)
+		print('Scanning port %s'% target_port)
 		connection_Scan(target_host, int(target_port))
 
 def main():
-	usage = 'usage %prog –h <target host> -p <target port>'
+	usage = '%prog –H <target host> -p <target port>'
 	parser = optparse.OptionParser(usage=usage)
-	parser.add_option('-h', dest='target_host', type='string',\
+	parser.add_option('-H', dest='target_host', type='string',\
 		help='specify target host')
-	parser.add_option('-p', dest='target_port', type='int',\
+	parser.add_option('-p', dest='target_port', type='string',\
 		help='specify target port[s]')
 	(options, args) = parser.parse_args()
 	target_host = options.target_host
-	target_port = options.target_port
+	target_port = str(options.target_port).split(',')
 	if (target_host == None) | (target_port == None):
 		print(parser.usage)
 		exit()
+	port_scan(target_host,target_port)
 
 
-if __name__ ==' __main__':
+if __name__ =='__main__':
 	main()
